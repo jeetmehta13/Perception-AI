1c08348ac75c3f82dd5587ef1eac2a0c32815f42
Ubuntu <azureuser@Perception-AI.jv5sawkyivxebij2oxkwyncqne.ix.internal.cloudapp.net>
2023-09-18 15:53:28 +0000

    necessary changes

--- app.py
+++ app.py
@@ -266,4 +266,6 @@ def process_data():
         return jsonify(output_data)    
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if __name__ == "__main__":
+	    app.run(host="127.0.0.1", port=8000)
+
